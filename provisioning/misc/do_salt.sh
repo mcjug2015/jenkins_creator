8ed577ed064a19ab17b6e12117620e1313a76f36
Victor Semenov <victor.semenov@gmail.com>
2016-05-14 12:43:05 -0400

    configing salt minion to recieve orders locally

--- provisioning/misc/do_salt.sh
+++ provisioning/misc/do_salt.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 
-# install saltstack globally, invoke saltstack top. this will be invoked by cloud init and vagrant.
+# install/config saltstack globally, invoke saltstack top. this will be invoked by cloud init and vagrant.
+# assumes a copy of jenkins creator lives under /tmp/jenkins_creator/
 /usr/bin/curl -o /tmp/epel-release.rpm http://mirror.sfo12.us.leaseweb.net/epel/7/x86_64/e/epel-release-7-6.noarch.rpm
 /usr/bin/rpm -Uvh /tmp/epel-release.rpm
 /usr/bin/yum install -y salt-minion
+/usr/bin/cp /tmp/jenkins_creator/provisioning/saltstack/minion /etc/salt/minion
